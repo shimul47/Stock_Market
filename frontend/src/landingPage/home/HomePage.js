7133250771238e1ec574e51c5b3d15a025be9d4d
shimul47 <oshimul54@gmail.com>
2025-06-27 19:45:59 +0600

    First Half of Home Page

---
+++ frontend/src/landingPage/home/HomePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Hero from "./Hero";
+
+import Stats from "./Stats";
+import Pricing from "./Pricing";
+import Education from "./Education";
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Stats />
+      <Pricing />
+      <Education />
+    </>
+  );
+}
+
+export default HomePage;

96dea3c345e2c0d1cfd4b861ad1255b725e52883
shimul47 <oshimul54@gmail.com>
2025-07-06 23:40:14 +0600

    Working on Dashboard

---
+++ dashboard/src/components/Positions.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { positions } from "../data/data";
+const Positions = () => {
+  return (
+    <>
+      <h3 className="title">Positions ({positions.length})</h3>
+
+      <div className="order-table">
+        <table>
+          <tr>
+            <th>Product</th>
+            <th>Instrument</th>
+            <th>Qty.</th>
+            <th>Avg.</th>
+            <th>LTP</th>
+            <th>P&L</th>
+            <th>Chg.</th>
+          </tr>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Positions;

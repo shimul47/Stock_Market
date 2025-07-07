13c4c21a725e22affee86a2f54b8dee3f1764f76
shimul47 <oshimul54@gmail.com>
2025-07-07 23:46:49 +0600

    watchlist updated

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,5 +1,7 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Tooltip, Grow } from "@mui/material";
+import { watchlist } from "../data/data";
+import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
 const WatchList = () => {
   return (
     <div className="watchlist-container">
@@ -11,12 +13,60 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts"> {watchlist.length} / 50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchlist.map((stock, index) => {
+          return <WatchListItem stock={stock} key={index} />;
+        })}
+      </ul>
     </div>
   );
 };
 
 export default WatchList;
+
+const WatchListItem = ({ stock }) => {
+  const [showWatchlistActions, setshowlistActions] = useState(false);
+  const handleMouseEnter = (e) => {
+    setshowlistActions(true);
+  };
+  const handleMouseExit = (e) => {
+    setshowlistActions(false);
+  };
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseExit}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itmeInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <KeyboardArrowDown className="down" />
+          ) : (
+            <KeyboardArrowUp className="up" />
+          )}
+          <span className="percent">{stock.price}</span>
+        </div>
+      </div>
+      {showWatchlistActions && <WatchlistActions uid={stock.name} />}
+    </li>
+  );
+};
+
+const WatchListActions = ({ uid }) => {
+  return (
+    <span className="actions">
+      <span>
+        <Tooltip
+          title="Buy (B)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="buy">Buy</button>
+        </Tooltip>
+      </span>
+    </span>
+  );
+};
